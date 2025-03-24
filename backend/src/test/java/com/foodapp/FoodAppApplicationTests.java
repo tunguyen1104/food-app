1844479ee80683aec3321ad36cab5f581557eae6
rappp <giap8547813@gmail.com>
2025-03-24 21:22:36 +0700

    feat: Config Test Class

--- backend/src/test/java/com/foodapp/FoodAppApplicationTests.java
+++ backend/src/test/java/com/foodapp/FoodAppApplicationTests.java
@@ -2,12 +2,21 @@ package com.foodapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class FoodAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> "jdbc:mysql://localhost:3306/food_app");
+        registry.add("spring.datasource.username", () -> "root");
+        registry.add("spring.datasource.password", () -> "root");
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
