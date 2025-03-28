e7a4719daafba16fb10fa7165085cdaeceb88fb1
tunguyen1104 <tu.pro5564@gmail.com>
2025-03-28 16:38:42 +0700

    feat: Replace setContentView with ViewBinding

--- android/app/src/main/java/com/example/foodapp/activities/MainActivity.java
+++ android/app/src/main/java/com/example/foodapp/activities/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.foodapp.activities;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.foodapp.R;
+import com.example.foodapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
