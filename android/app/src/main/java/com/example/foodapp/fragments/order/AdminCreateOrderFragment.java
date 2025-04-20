158aa68a8ae8a337db7f0394cfb464696b810213
tunguyen1104 <tu.pro5564@gmail.com>
2025-04-20 21:42:18 +0700

    chore: remove unused imports

--- android/app/src/main/java/com/example/foodapp/fragments/order/AdminCreateOrderFragment.java
+++ android/app/src/main/java/com/example/foodapp/fragments/order/AdminCreateOrderFragment.java
@@ -1,10 +1,5 @@
 package com.example.foodapp.fragments.order;
 
-import static com.example.foodapp.consts.Constants.PICK_IMAGE_REQUEST;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +10,6 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
